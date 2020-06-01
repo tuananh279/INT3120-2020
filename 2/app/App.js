cb0f948eb082a10a6a50696c3422de206591c25b
tuananh279 <43823554+tuananh279@users.noreply.github.com>
2020-06-02 00:13:27 +0700

    Sửa thanh StatusBar
    Thay đổi màu của thanh StatusBar

--- 2/app/App.js
+++ 2/app/App.js
@@ -1,11 +1,16 @@
 // Nguyen Tuan Anh 17020585
 
 import React, {Component} from 'react';
+import {StatusBar, View } from 'react-native';
 import Categories from './screens/Categories';
-import firebase from '@react-native-firebase/app';
 
 export default class App extends Component {
   render() {
-    return <Categories />;
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar backgroundColor="#12a6e4"/>
+        <Categories />
+      </View>
+    );
   }
 }
